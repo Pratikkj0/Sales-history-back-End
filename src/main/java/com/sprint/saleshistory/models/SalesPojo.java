b7e82aa91240aa898657c57636fe36bf593a37b5
SMAPPETI <SOMANADH.MAPPETI@CAPGEMINI.COM>
2023-12-13 09:38:51 +0530

    Changes done in SalesController, SalesPojo and removed timesCongtroller, timesService layer

--- src/main/java/com/sprint/saleshistory/models/SalesPojo.java
+++ src/main/java/com/sprint/saleshistory/models/SalesPojo.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-
 @Getter
 @Setter
 @NoArgsConstructor
@@ -15,26 +14,22 @@ import lombok.ToString;
 @ToString
 
 public class SalesPojo {
-	
-		@NotNull
-	    private int id;
-		@NotNull
-		private ProductPojo productPojo;
-		@NotNull
-		private CustomerPojo customerPojo;
-		@NotNull
-		private TimesPojo timesPojo;
-		@NotNull
-		private ChannelPojo channelPojo;
-		@NotNull
-		private PromotionPojo promotionPojo;
-		@NotNull
-		private int quantitySold;
-		@NotNull
-		private Double amountSold;
-	   
+
+	@NotNull
+	private int id;
+	@NotNull
+	private ProductPojo productPojo;
+	@NotNull
+	private CustomerPojo customerPojo;
+	@NotNull
+	private TimesPojo timesPojo;
+	@NotNull
+	private ChannelPojo channelPojo;
+	@NotNull
+	private PromotionPojo promotionPojo;
+	@NotNull
+	private int quantitySold;
+	@NotNull
+	private Double amountSold;
 
 }
-
-	
-
